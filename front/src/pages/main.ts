4f1e49f8768d89917d326ce2f0b2012be476cd89
Ryota Shimizu <shimizu_ryota@giginc.co.jp>
2023-12-20 13:04:13 +0900

    fix: format

--- front/src/pages/main.ts
+++ front/src/pages/main.ts
@@ -1,7 +1,7 @@
-import "destyle.css";
-import "../scss/index.scss"
+import 'destyle.css'
+import '../scss/index.scss'
 
-import { commonInit } from "../ts/common";
+import { commonInit } from '../ts/common'
 
 console.log('main.ts')
 commonInit()
