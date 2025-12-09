0a522ee8391654a6a8219f27e422434689e398fc
Ryota Shimizu <shimizu_ryota@giginc.co.jp>
2025-12-09 18:18:50 +0900

    fix: fluid

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import relativeLinks from "astro-relative-links";
 import autoprefixer from "autoprefixer";
 import glsl from 'vite-plugin-glsl';
-import vitePluginString from 'vite-plugin-string';
 
 // https://astro.build/config
 export default defineConfig({
   outDir: "./dist",
+  output: 'server',
+  adapter: vercel(),
   build: {
     format: "file",
   },
